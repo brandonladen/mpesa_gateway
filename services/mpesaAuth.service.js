2664f67ad866193e18a5c12244e4b6dd0726a8ad
William-nyarash <owetywilliamnyaranga@gmail.com>
2025-08-15 15:44:23 +0300

    fix import for the variables

--- services/mpesaAuth.service.js
+++ services/mpesaAuth.service.js
@@ -1,28 +1,24 @@
 // OAuth token logic
 const axios = require("axios");
-const path = require("path");
 const logger = require("../utils/logger");
 
-require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
-require("dotenv").config();
 
-const generateAccessToken = async () => {
-  const consumerKey = process.env.MPESA_CONSUMER_KEY;
-  const consumerSecret = process.env.MPESA_CONSUMER_SECRET;
+const generateAccessToken =async ()=> {
+  const {
+      CONSUMER_KEY,
+      CONSUMER_SECRET,
+      MPESA_BASE_URL
+  } = require("../config/env.js");
 
-  const url =
-    "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
+    const url = `${MPESA_BASE_URL}/oauth/v1/generate?grant_type=client_credentials`;
 
   try {
-    const encodedCredentials = Buffer.from(
-      `${consumerKey}:${consumerSecret}`
-    ).toString("base64");
+    const encodedCredentials = Buffer.from(`${CONSUMER_KEY}:${CONSUMER_SECRET}`).toString('base64');
 
     const headers = {
-      Authorization: `Basic ${encodedCredentials}`,
-      "Content-Type": "application/json",
+      'Authorization': `Basic ${encodedCredentials}`,
+      'Content-Type': 'application/json'
     };
-
     const response = await axios.get(url, { headers });
     const token = response.data.access_token;
     return token;
