cf67096c75beed703393eaf736d6ae6766f39571
William-nyarash <owetywilliamnyaranga@gmail.com>
2025-08-15 15:53:00 +0300

    fix import

--- services/mpesaAuth.service.js
+++ services/mpesaAuth.service.js
@@ -1,1 +1,36 @@
 // OAuth token logic
+const axios = require("axios");
+const logger = require("../utils/logger");
+
+
+const generateAccessToken =async ()=> {
+  const {
+      CONSUMER_KEY,
+      CONSUMER_SECRET,
+      MPESA_BASE_URL
+  } = require("../config/env.js");
+
+    const url = `${MPESA_BASE_URL}/oauth/v1/generate?grant_type=client_credentials`;
+    console.log("the url",url)
+  try {
+    const encodedCredentials = Buffer.from(`${CONSUMER_KEY}:${CONSUMER_SECRET}`).toString('base64');
+
+    const headers = {
+      'Authorization': `Basic ${encodedCredentials}`,
+      'Content-Type': 'application/json'
+    };
+    const response = await axios.get(url, { headers });
+    console.log("access", response.data)
+    const token = response.data.access_token;
+    return token;
+  } catch (error) {
+    logger.error("Error fetching M-Pesa access token", {
+      status: error.response?.status,
+      data: error.response?.data,
+      message: error.message
+    });
+    throw new Error("Failed to get access token.");
+  }
+};
+generateAccessToken()
+// module.exports = { generateAccessToken };
